86df5e0b7a0808557861958fe10b36b367d25f51
Baróczi Gábor <yaga18@gmail.com>
2019-07-03 17:26:07 +0200

    I'm dumb and two last commits were wrong ones. Reuploading the original code

--- ButtonHandler.cpp
+++ ButtonHandler.cpp
@@ -1,101 +1,31 @@
-// 
-// 
-// 
+#include "ButtonHandler.h"
 
-#include "ArduinoRotaryHandler.h"
-
-// Get the 
-ArduinoRotaryHandler * ArduinoRotaryHandler::getInstance() {
-	return &instance;
-}
-
-void ArduinoRotaryHandler::initButton(const uint8_t pin, const uint16_t debounceDelay = 100, const uint16_t longPressDelay = 1000) {
+// Initialize button
+void ButtonHandlerClass::initButton(const uint8_t pin, const uint16_t debounceDelay = 100, const uint16_t longPressTime = 1000) {
 	if (0 <= pin && pin < NUM_DIGITAL_PINS) {
 		pinMode(pin, INPUT);
-		p_buttonPortRegisterAddress = portInputRegister(digitalPinToPort(pin));
-		m_buttonPin = pin;
-		DPRINT(F("Button Pin: "));
-		DPRINTLN(m_buttonPin);
-		DPRINT(F("Button Pin Port number: "));
-		DPRINTLN(digitalPinToPort(m_buttonPin));
-		DPRINT(F("Button Pin Port register address: "));
-		DPRINTLN((uint16_t)p_buttonPortRegisterAddress);
+		p_portRegisterAddress = portInputRegister(digitalPinToPort(pin));
+		m_pin = pin;
+		DPRINT_BUT(String(F("Initializing button on pin: ")) + String(m_pin));
+		DPRINT_BUT(String(F("Port number ")) + String(digitalPinToPort(m_pin)));
+		DPRINT_BUT(String(F("Port register address: ")) + String((uint8_t)p_portRegisterAddress));
 
 		m_debounceDelay = debounceDelay;
-		m_longPressDelay = longPressDelay;
+		m_longPressDelay = longPressTime;
 		m_buttonInitialized = true;
-		DPRINT(F("Button initialized on pin: "));
-		DPRINTLN(m_buttonPin);
+		DPRINT_BUT(String(F("Button initialized")));
 	}
 	else {
 		m_buttonInitialized = false;
-		DPRINTLN(F("Failed to initialize button, wrong pin given"));
+		DPRINT_BUT(F("Failed to initialize button, wrong pin given"));
 	}
 }
 
-void ArduinoRotaryHandler::initRotary(const uint8_t interrupt1Pin, const uint8_t interrupt2Pin) {
-	if (digitalPinToInterrupt(interrupt1Pin) >= 0 && digitalPinToInterrupt(interrupt2Pin) >= 0 && interrupt1Pin != interrupt2Pin) {
-		p_rotaryInterrupt1PortRegisterAddress = portInputRegister(digitalPinToPort(interrupt1Pin));
-		p_rotaryInterrupt2PortRegisterAddress = portInputRegister(digitalPinToPort(interrupt2Pin));
-		m_rotaryInterrputPin1 = interrupt1Pin;
-		m_rotaryInterrputPin2 = interrupt2Pin;
-#ifdef ROTARY_TRIGGER_RISING
-		attachInterrupt(digitalPinToInterrupt(interrupt1Pin), f_ISR1Wrapper, RISING);
-		attachInterrupt(digitalPinToInterrupt(interrupt2Pin), f_ISR2Wrapper, RISING);
-#endif
-#ifdef ROTARY_TRIGGER_FALLING
-		attachInterrupt(digitalPinToInterrupt(interrupt1Pin), f_ISR1Wrapper, FALLING);
-		attachInterrupt(digitalPinToInterrupt(interrupt2Pin), f_ISR2Wrapper, FALLING);
-#endif
-		DPRINT(F("Interrupt 1 Pin: "));
-		DPRINTLN(m_rotaryInterrputPin1);
-		DPRINT(F("Interrupt 2 Pin: "));
-		DPRINTLN(m_rotaryInterrputPin2);
-		DPRINT(F("Interrupt 1 Pin port number:"));
-		DPRINTLN(digitalPinToPort(interrupt1Pin));
-		DPRINT(F("Interrupt 2 Pin port number:"));
-		DPRINTLN(digitalPinToPort(interrupt2Pin));
-		DPRINT(F("Interrupt 1 Pin port register address: "));
-		DPRINTLN((uint16_t)p_rotaryInterrupt1PortRegisterAddress);
-		DPRINT(F("Interrupt 2 Pin port register address: "));
-		DPRINTLN((uint16_t)p_rotaryInterrupt2PortRegisterAddress);
-		DPRINTLN(F("Rotary encoder initialized"));
-		m_rotaryInitialized = true;
-	}
-	else {
-		DPRINTLN(F("Failed to initialize rotary encoder, given pins are not interrupt capable"));
-		m_rotaryInitialized = false;
-	}
-}
-
-void ArduinoRotaryHandler::handleRotary(void(*callback1)(), void(*callback2)()) {
-	if (m_rotaryInitialized) {
-		switch (m_rotaryEvent) {
-		case RE_DOWN:
-			DPRINTLN(F("callback1 EVENT"));
-			callback1();
-			m_rotaryEvent = RE_NULL;
-			break;
-		case RE_NULL:
-			break;
-		case RE_UP:
-			DPRINTLN(F("callback2 EVENT"));
-			callback2();
-			m_rotaryEvent = RE_NULL;
-			break;
-		}
-	}
-}
-
-void ArduinoRotaryHandler::handleButton(void(*shortPress)()) {
+// Handle button with short press event
+void ButtonHandlerClass::handleButton(void(*shortPress)()) {
 	if (m_buttonInitialized) {
 		// read the value of the button
-#ifdef BUTTON_ACTIVE_HIGH
-		bool buttonState = bitRead(*p_buttonPortRegisterAddress, m_buttonPin);
-#endif
-#ifdef BUTTON_ACTIVE_LOW
-		bool buttonState = !bitRead(*p_buttonPortRegisterAddress, m_buttonPin);
-#endif
+		bool buttonState = bitRead(*p_portRegisterAddress, m_pin);
 
 		// check for debounce timer and if m_enabled after long press
 		if ((millis() - m_debounceLastTime > m_debounceDelay) && m_enabled) {
@@ -104,10 +34,9 @@ void ArduinoRotaryHandler::handleButton(void(*shortPress)()) {
 				// short press event
 				if (buttonState == false) {
 					m_hasBeenPressed = false;
-					m_rotaryEvent = RE_NULL;
 					if (shortPress != nullptr) shortPress();
 					//m_enabled = false;
-					DPRINTLN(F("Button short click, without longpress"));
+					DPRINT_BUT(F("Button short click, without longpress"));
 					return;
 				}
 			}
@@ -122,15 +51,11 @@ void ArduinoRotaryHandler::handleButton(void(*shortPress)()) {
 	}
 }
 
-void ArduinoRotaryHandler::handleButton(void(*shortPress)(), void(*longPress)()) {
+// Handle button with short press and long press event
+void ButtonHandlerClass::handleButton(void(*shortPress)(), void(*longPress)()) {
 	if (m_buttonInitialized) {
 		// read the value of the button
-#ifdef BUTTON_ACTIVE_HIGH
-		bool buttonState = bitRead(*p_buttonPortRegisterAddress, m_buttonPin);
-#endif
-#ifdef BUTTON_ACTIVE_LOW
-		bool buttonState = !bitRead(*p_buttonPortRegisterAddress, m_buttonPin);
-#endif
+		bool buttonState = bitRead(*p_portRegisterAddress, m_pin);
 
 		// check for debounce timer and if m_enabled after long press
 		if ((millis() - m_debounceLastTime > m_debounceDelay) && m_enabled) {
@@ -139,19 +64,17 @@ void ArduinoRotaryHandler::handleButton(void(*shortPress)(), void(*longPress)())
 				// short press event
 				if (buttonState == false) {
 					m_hasBeenPressed = false;
-					m_rotaryEvent = RE_NULL;
 					if (shortPress != nullptr) shortPress();
 					//m_enabled = false;
-					DPRINTLN(F("Button short click"));
+					DPRINT_BUT(F("Button short click"));
 					return;										// to bypass longPress
 				}
 				// long press event
 				if ((buttonState == true) && ((millis() - m_longPressTimer) > m_longPressDelay)) {
 					m_hasBeenPressed = false;
-					m_rotaryEvent = RE_NULL;
 					if (longPress != nullptr) longPress();
 					m_enabled = false;							// disable button to prevent another press event
-					DPRINTLN(F("Button long click"));
+					DPRINT_BUT(F("Button long click"));
 				}
 			}
 			else {
@@ -164,38 +87,6 @@ void ArduinoRotaryHandler::handleButton(void(*shortPress)(), void(*longPress)())
 			if (buttonState == false) m_enabled = true;
 		}
 	}
-
 }
 
-void ArduinoRotaryHandler::f_ISR1Wrapper() {
-#ifdef ROTARY_TRIGGER_RISING
-	if (!bitRead(*(instance.p_rotaryInterrupt2PortRegisterAddress), instance.m_rotaryInterrputPin2)) {
-#endif
-#ifdef ROTARY_TRIGGER_FALLING
-		if (bitRead(*(instance.p_rotaryInterrupt2PortRegisterAddress), instance.m_rotaryInterrputPin2)) {
-#endif
-			instance.f_ISR1();
-		}
-	};
-
-	void ArduinoRotaryHandler::f_ISR2Wrapper() {
-#ifdef ROTARY_TRIGGER_RISING
-		if (!bitRead(*(instance.p_rotaryInterrupt1PortRegisterAddress), instance.m_rotaryInterrputPin1)) {
-#endif
-#ifdef ROTARY_TRIGGER_FALLING
-			if (bitRead(*(instance.p_rotaryInterrupt1PortRegisterAddress), instance.m_rotaryInterrputPin1)) {
-#endif
-				instance.f_ISR2();
-			}
-		};
-
-		void ArduinoRotaryHandler::f_ISR1() {
-			m_rotaryEvent = RE_DOWN;
-		}
-
-		void ArduinoRotaryHandler::f_ISR2() {
-			m_rotaryEvent = RE_UP;
-		}
-
-		ArduinoRotaryHandler ArduinoRotaryHandler::instance = {};
-
+ButtonHandlerClass ButtonHandler;
--- ButtonHandler.h
+++ ButtonHandler.h
@@ -1,133 +1,42 @@
-﻿// ArduinoRotaryHandler.h
+﻿// ButtonHandler.h
 
-#ifndef _ARDUINOROTARYHANDLER_h
-#define _ARDUINOROTARYHANDLER_h
+#ifndef _BUTTONHANDLER_h
+#define _BUTTONHANDLER_h
 
 #include "arduino.h"
 
-/*================================================================//
-================= USAGE ==========================================//
-==================================================================//
-																  //
-//In main code:													  //
-#include "ArduinoRotaryHandler.h"								  //
-																  //
-ArduinoRotaryHandler *rh = ArduinoRotaryHandler::getInstance();	  //
-void setup() {													  //
-	rh->initButton(PD4);										  //
-	rh->initRotary(PD2, PD3);									  //
-}																  //
-																  //
-void loop() {													  //
-	rh->handleButton([]() {});									  //
-}																  //
-																  //
-//In class:														  //
-#include "ArduinoRotaryHandler.h"								  //
-																  //
-class TestClass {												  //
-private:														  //
-	friend class ArduinoRotaryHandler;							  //
-	ArduinoRotaryHandler* rh;									  //
-public:															  //
-	TestClass() {												  //
-		rh = ArduinoRotaryHandler::getInstance();				  //
-	}															  //
-																  //
-	void init() {												  //
-		rh->initButton(PD4);									  //
-		rh->initRotary(PD2, PD3);								  //
-	}															  //
-																  //
-};																  //
-==================================================================*/
+// uncomment this for debugging
+//#define DEBUG_BUT
 
-/*=========================================*/
-/*=============== CONFIG ==================*/
-/*=========================================*/
-// ===== Set rotary logic level ===========//
-										   //
-// Interrupt set to RISING trigger	       //
-//#define ROTARY_TRIGGER_RISING		       //
-										   //
-// Interrupt set to FALLING trigger	       //
-#define ROTARY_TRIGGER_FALLING		       //
-// ========================================//
-
-// ===== Set button logic level ==============================//
-															  //
-//#define BUTTON_ACTIVE_HIGH								  //
-															  //
-#define BUTTON_ACTIVE_LOW									  //
-															  //
-/* ===== Button circuit =====								  //
-   ===== Active high =====			===== Active low =====	  //
-	   ___ Vcc 						   ___ Vcc				  //
-		|								|					  //
-	   / 							  |‾‾‾|					  //
-	  /  							  | R |					  //
-		|							   ‾|‾					  //
-		|—————— uController				|—————— uController	  //
-		|								|					  //
-	  |‾‾‾|							   /					  //
-	  | R |							  /						  //
-	   ‾|‾							    |					  //
-	   ‾‾‾ GND						   ‾‾‾ GND				  //
-*/															  //
-// ===========================================================//
-
-#define DEBUG_ROTARY
-#ifdef DEBUG_ROTARY
-#define DPRINTLN(x) Serial.println(x)
-#define DPRINT(x) Serial.print(x)
+#ifdef DEBUG_BUT
+#define DPRINT_BUT(x) Serial.println(x);
 #else
-#define DPRINTLN(x)
-#define DPRINT(x)
+#define DPRINT_BUT(x)
 #endif
 
-enum rotaryEvent_e {
-	RE_DOWN = -1,
-	RE_NULL = 0,
-	RE_UP = 1
-};
 
-class ArduinoRotaryHandler {
+class ButtonHandlerClass {
 private:
-	//===== ELSE =====
-	static ArduinoRotaryHandler instance;
-	ArduinoRotaryHandler() {};
-
-	//===== ROTARY =====
-	bool m_rotaryInitialized;
-	uint8_t m_rotaryInterrputPin1;
-	uint8_t m_rotaryInterrputPin2;
-	volatile rotaryEvent_e m_rotaryEvent;
-	volatile uint8_t *p_rotaryInterrupt1PortRegisterAddress;
-	volatile uint8_t *p_rotaryInterrupt2PortRegisterAddress;
-
-	void f_ISR1();
-	void f_ISR2();
-	static void f_ISR1Wrapper();
-	static void f_ISR2Wrapper();
-
-	//===== BUTTON =====
-	bool m_buttonInitialized;
 	bool m_enabled;
 	bool m_hasBeenPressed;
+
 	uint16_t m_debounceDelay;
 	uint32_t m_debounceLastTime;
+
 	uint16_t m_longPressDelay;
 	uint32_t m_longPressTimer;
-	uint8_t m_buttonPin;
-	volatile uint8_t *p_buttonPortRegisterAddress;
+
+	bool m_buttonInitialized;
+	volatile uint8_t *p_portRegisterAddress;
+	uint8_t m_pin;
 
 public:
-	static ArduinoRotaryHandler *getInstance();
-	void initButton(const uint8_t pin, const uint16_t debounceDelay = 100, const uint16_t longPressDelay = 1000);
-	void initRotary(const uint8_t interrupt1Pin, const uint8_t interrupt2Pin);
-	void handleRotary(void(*callback1)(), void(*callback2)());
+	void initButton(const uint8_t pin, const uint16_t debounceDelay = 100, const uint16_t longPressTime = 1000);
 	void handleButton(void(*shortPress)());
 	void handleButton(void(*shortPress)(), void(*longPress)());
+
 };
+
+extern ButtonHandlerClass ButtonHandler;
 
 #endif
